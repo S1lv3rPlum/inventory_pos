1f1ea7a94c69319000c853a31510e2eb11f1fa1e
S1lv3rPlum <Reneeslueke@gmail.com>
2025-07-22 18:56:44 -0400

    update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,44 @@
-self.addEventListener("install", (event) => {
-  console.log("[Service Worker] Install");
-  self.skipWaiting(); // Activate worker immediately
+const CACHE_NAME = "inventory-pos-cache-v3";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./inventory.html",
+  "./inventory.js",
+  "./BrowseProducts.html",
+  "./browse.js",
+  "./backup.js",
+  "./colortheme.js",
+  "./menu-overlay.js",
+  "./payment.js",
+  "./POS.html",
+  "./pos.js",
+  "./sales.html",
+  "./sales.js",
+  "./Settings.html",
+  "./ShippingManager.html",
+  "./ShippingManager.js"
+];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+  self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("[Service Worker] Activate");
-  self.clients.claim(); // Become available to all pages
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+    )
+  );
+  self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  // Simple pass-through handler for now
-  event.respondWith(fetch(event.request).catch(() => {
-    return new Response("Offline", { status: 503, statusText: "Offline" });
-  }));
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(
+    caches.match(evt.request).then(resp => resp || fetch(evt.request).catch(() => caches.match("./")))
+  );
 });
