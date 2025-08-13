a89a81da263a2370c6a7c6d6d4139aa274528b80
S1lv3rPlum <Reneeslueke@gmail.com>
2025-08-13 15:28:54 -0500

    update inventory2.js

--- inventory2.js
+++ inventory2.js
@@ -141,8 +141,10 @@ function renderProducts() {
                 <td>${sizeInputs}</td>
                 <td>
                     <input type="file" accept="image/*" class="row-image-input"><br>
-                    <button class="save-btn" data-index="${product.index}">Save</button>
-                    <button class="delete-btn" data-index="${product.index}">Delete</button>
+                    
+        <button class="save-btn" data-index="${idx}">Save</button>
+        <button class="delete-btn" data-index="${idx}">Delete</button>
+    `;
                 </td>
             `;
 
